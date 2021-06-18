dd79a30f4d5a1faea8647b7fae3a5b4e7aedede1
Audu Billy <audubilly89@gmail.com>
2021-06-18 22:51:48 +0100

    created the login, logout & signup pages

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,35 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Post
 
+
+
 class BlogListView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'blog/post_home.html'
+
 
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name= 'blog/post_new.html'
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ['title','body']
+
+
+class BlogDeleteView(DeleteView):
+        model= Post
+        template_view= 'blog/post_confirm_delete.html'
+        success_url = reverse_lazy('post_home')
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'post_home'
+LOGOUT_REDIRECT_URL = 'post_home'
